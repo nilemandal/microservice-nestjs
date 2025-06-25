5410504c704d13cbf7dee6d8b895f4eaa742b8bd
Nilesh Mandal <nilemandal@yahoo.com>
2025-06-25 17:25:48 +0530

    feat: add husky and lint-staged for pre-commit hooks, update lint-staged configuration

--- apps/jobber-auth-e2e/src/support/global-teardown.ts
+++ apps/jobber-auth-e2e/src/support/global-teardown.ts
@@ -1,11 +1,10 @@
 import { killPort } from '@nx/node/utils';
-/* eslint-disable */
 
 module.exports = async function () {
   try {
     // Put clean up logic here (e.g. stopping services, docker-compose, etc.).
     // Hint: `globalThis` is shared between setup and teardown.
-    
+
     if (globalThis.__SERVE_PROCESS__) {
       console.log('Stopping the service...');
       globalThis.__SERVE_PROCESS__.kill('SIGINT');
