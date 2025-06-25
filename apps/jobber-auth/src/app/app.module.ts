d2d3a0ace1ba82964320092f92ca324d25164e0d
Nilesh Mandal <nilemandal@yahoo.com>
2025-06-25 11:59:48 +0530

    chore: update prisma and @prisma/client to version 6.10.1, add g and nx dependencies, and configure paths in tsconfig.base.json

--- apps/jobber-auth/src/app/app.module.ts
+++ apps/jobber-auth/src/app/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { PrismaModule } from './prisma/prisma.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true
+    })
+  ],
   controllers: [AppController],
-  providers: [],
+  providers: [
+    PrismaModule
+  ],
 })
 export class AppModule {}
