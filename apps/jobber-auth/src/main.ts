f290480c1aecd11f6ce06744b38ec996bde3f9bd
Nilesh Mandal <nilemandal@yahoo.com>
2025-06-25 17:25:16 +0530

    test commit hooks

--- apps/jobber-auth/src/main.ts
+++ apps/jobber-auth/src/main.ts
@@ -10,7 +10,7 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
-  // app.useGlobalPipes(new ValidationPipe({whitelist: true}))
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
   await app.listen(port);
