6733e63dfeec1a9a126f42a28397aec9b0a236e3
Nilesh Mandal <nilemandal@yahoo.com>
2025-06-26 22:35:23 +0530

    test: enhance AuthResolver and AuthService tests with additional provider mocks

--- apps/jobber-auth/src/app/auth/auth.service.spec.ts
+++ apps/jobber-auth/src/app/auth/auth.service.spec.ts
@@ -1,12 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '../users/users.service';
+import { ConfigService } from '@nestjs/config';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: ConfigService,
+          useValue: {},
+        },
+        {
+          provide: JwtService,
+          useValue: {},
+        },
+        {
+          provide: UsersService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
